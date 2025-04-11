6de3091351cf797748a52aa624321b4d0a385888
Taras <taras@coderiver.com.ua>
2025-04-11 18:59:25 +0300

    added menu

--- src/main.js
+++ src/main.js
@@ -2,9 +2,26 @@ import './scss/style.scss';
 
 const { body } = document;
 const burger = body.querySelector('.burger');
-const line = burger.querySelector('.burger__line');
+const burgerLine = burger.querySelector('.burger__line');
+const menu = body.querySelector('.page__menu');
+
+let menuIsOpened = false;
 
 burger.addEventListener('click', () => {
-  burger.classList.toggle('burger--active');
-  line.classList.toggle('burger__line--active');
+
+  if (!menuIsOpened) {
+    menuIsOpened = true;
+    burger.classList.add('burger--active');
+    burgerLine.classList.add('burger__line--active');
+    menu.classList.add('page__menu--active');
+    body.style.overflow = 'hidden';
+
+    return;
+  }
+
+  menuIsOpened = false;
+  burger.classList.remove('burger--active');
+  burgerLine.classList.remove('burger__line--active');
+  menu.classList.remove('page__menu--active');
+  body.style.overflow = '';
 });
