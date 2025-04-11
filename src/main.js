a14de7fda34753cc943e8e3e2483ebff1c89c6dc
Taras <taras@coderiver.com.ua>
2025-04-11 13:46:05 +0300

    added header

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
 import './scss/style.scss';
+
+const { body } = document;
+const burger = body.querySelector('.burger');
+const line = burger.querySelector('.burger__line');
+
+burger.addEventListener('click', () => {
+  burger.classList.toggle('burger--active');
+  line.classList.toggle('burger__line--active');
+});
